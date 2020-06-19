4e54046945a5b946495c3f557c47506792d667bb
Mayur Borse <mayur@hyphenos.io>
2020-06-19 13:05:53 +0530

    Styling added for blog pages
    - All archive posts listed on single page
    - CODE_COLOR_SCHEME set to 'friendly' in conf.py (blue and green)
    - Blockquotes (>) removed from prev posts (crud-pattern)
    - border-radius-bottom of .2em added to blog-card
    - background-color: aliceblue removed from .blog-header
    - hyphenOs-blue used as color for .blog-header
    - .blog-content font-size reduced to 1.2em from 1.4em
    - Tags moved to subheader & added on blog index page
    - Templates added for author, tag, tags & archive pages
    - .chip-container contents will be shown in column in mobile view
    - .listdate font-size is .7rem in mobile view. It is used for date
    
    Links with styling added on blog page
    
    - class .blog-container added to blog page container
    - class .blog-links added to links container along
      with .blog-header, .blog-card classes
    - class .blog-link added to each link
    - class .blue added to anchor <a> of each blog-link
    - Links added for Archive "/archive.html" & Categories "/categories"

--- conf.py
+++ conf.py
@@ -560,7 +560,7 @@ FRONT_INDEX_HEADER = {
 # CREATE_SINGLE_ARCHIVE = False
 # Create year, month, and day archives each with a (long) list of posts
 # (overrides both CREATE_MONTHLY_ARCHIVE and CREATE_SINGLE_ARCHIVE)
-# CREATE_FULL_ARCHIVES = False
+CREATE_FULL_ARCHIVES = True
 # If monthly archives or full archives are created, adds also one archive per day
 # CREATE_DAILY_ARCHIVE = False
 # Create previous, up, next navigation links for archives
@@ -885,7 +885,7 @@ IMAGE_FOLDERS = {'images': 'images'}
 # This list MAY be incomplete since pygments adds styles every now and then.
 # Check with list(pygments.styles.get_all_styles()) in an interpreter.
 #
-# CODE_COLOR_SCHEME = 'default'
+CODE_COLOR_SCHEME = 'friendly'
 
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
