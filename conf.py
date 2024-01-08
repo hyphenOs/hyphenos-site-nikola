6ba5804d7b3af77cdc33aa6520813cf78e7acd71
Abhijit Gadgil <gabhijit@hyphenos.io>
2024-01-08 15:15:27 +0530

    Updated the Logo for the site (light version)

--- conf.py
+++ conf.py
@@ -345,7 +345,7 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = '/assets/img/hyphenOs_256x64.png'
+LOGO_URL = '/assets/img/hyphenos-logo-is-text-light.png'
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
